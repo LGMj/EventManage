a63adac055db4ba1c0cc75bfadadab966cf84fe8
lgmj <1016032803@qq.com>
2021-05-30 11:28:36 +0800

    :wrench: add CMakeLists and support of vscode build

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/event_manager.o"
+  "CMakeFiles/main.dir/event_test.o"
+  "CMakeFiles/main.dir/led.o"
+  "CMakeFiles/main.dir/main.o"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
